b7396f664211b54b80cac9b772d8da4d06170ead
Jakub Skiba <kubask03@gmail.com>
2022-11-09 22:45:14 +0100

    print strings

--- src/boot.asm
+++ src/boot.asm
@@ -1,28 +1,37 @@
-mov bx, 30
+[org 0x7c00]
 
-cmp bx, 4
-jle first
-cmp bx, 40
-jl second
-jmp third
+mov bx, HELLO_MSG
+call print_string
 
-first:
-    mov al, 'A'
-    jmp then
+mov bx, SPACE
+call print_string
 
-second:
-    mov al, 'B'
-    jmp then
+mov bx, GOODBYE_MSG
+call print_string
 
-third:
-    mov al, 'C'
-    jmp then
+jmp $
 
-then:
+print_string:
+    mov al, [bx]
+    cmp al, 0
+    jne print_char
+    ret
+
+print_char:
     mov ah, 0x0e
     int 0x10
 
-    jmp $
+    add bx, 1
+    jmp print_string
+
+HELLO_MSG:
+    db "Hello world", 0
+
+GOODBYE_MSG:
+    db "Goodbye world", 0
+
+SPACE:
+    db " ", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
