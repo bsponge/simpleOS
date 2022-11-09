8f65c51782b5333ff4c0c7b3e9b651af821a6c99
Jakub Skiba <kubask03@gmail.com>
2022-11-09 22:18:04 +0100

    if else statement

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
-build:
+build-binary:
 	mkdir -p build
 	nasm src/boot.asm -f bin -o build/boot.bin
+	cp build/boot.bin /mnt/c/Program\ Files/qemu/
--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,28 @@
-loop:
-jmp loop
+mov bx, 30
 
+cmp bx, 4
+jle first
+cmp bx, 40
+jl second
+jmp third
+
+first:
+    mov al, 'A'
+    jmp then
+
+second:
+    mov al, 'B'
+    jmp then
+
+third:
+    mov al, 'C'
+    jmp then
+
+then:
+    mov ah, 0x0e
+    int 0x10
+
+    jmp $
 
 times 510-($-$$) db 0
-
-
 dw 0xaa55
