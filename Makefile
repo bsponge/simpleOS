d9f300d2c32bc6ed3eb28a614d80165144d398e6
Jakub Skiba <kubask03@gmail.com>
2022-11-10 17:55:29 +0100

    split boot.asm

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 build-binary:
 	mkdir -p build
-	nasm src/boot.asm -f bin -o build/boot.bin
+	nasm src/boot.asm -i src -f bin -o build/boot.bin
 	cp build/boot.bin /mnt/c/Program\ Files/qemu/
---
+++ src/print.asm
@@ -0,0 +1,47 @@
+print_string:
+    mov al, [bx]
+    cmp al, 0
+    jne print_char
+    ret
+
+print_char:
+    mov ah, 0x0e
+    int 0x10
+
+    add bx, 1
+    jmp print_string
+
+print_hex_16bit:
+    pusha
+
+    mov cx, 4
+again:
+    cmp cx, 0
+    jne shift_and_write
+    mov bx, HEX_OUT
+    call print_string
+    popa
+    ret
+shift_and_write:
+    sub cx, 1
+    mov ax, dx
+    and ax, 0x000f
+    cmp al, 9
+    jle number
+    jmp char
+back:
+    shr dx, 4
+    mov bx, HEX_OUT
+    add bx, 2
+    add bx, cx
+    mov [bx], al
+    jmp again
+number:
+    add ax, 48
+    jmp back
+char:
+    add ax, 55
+    jmp back
+
+HEX_OUT:
+    db "0x0000", 0
