506d7d773c0e968f34cfb8977b1909001ae21f9c
Jakub Skiba <kubask03@gmail.com>
2022-11-10 15:43:54 +0100

    print numbers in hex

--- src/boot.asm
+++ src/boot.asm
@@ -1,13 +1,7 @@
 [org 0x7c00]
 
-mov bx, HELLO_MSG
-call print_string
-
-mov bx, SPACE
-call print_string
-
-mov bx, GOODBYE_MSG
-call print_string
+mov dx, 0xbeef
+call print_hex_16bit
 
 jmp $
 
@@ -24,14 +18,40 @@ print_char:
     add bx, 1
     jmp print_string
 
-HELLO_MSG:
-    db "Hello world", 0
+print_hex_16bit:
+    pusha
 
-GOODBYE_MSG:
-    db "Goodbye world", 0
+    mov cx, 4
+again:
+    cmp cx, 0
+    jne shift_and_write
+    mov bx, HEX_OUT
+    call print_string
+    popa
+    ret
+shift_and_write:
+    sub cx, 1
+    mov ax, dx
+    and ax, 0x000f
+    cmp al, 9
+    jle number
+    jmp char
+back:
+    shr dx, 4
+    mov bx, HEX_OUT
+    add bx, 2
+    add bx, cx
+    mov [bx], al
+    jmp again
+number:
+    add ax, 48
+    jmp back
+char:
+    add ax, 55
+    jmp back
 
-SPACE:
-    db " ", 0
+HEX_OUT:
+    db "0x0000", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
