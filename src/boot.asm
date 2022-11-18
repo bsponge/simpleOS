70cffc59ec8f89b6a07da9cb69a493ef60536228
Jakub Skiba <kubask03@gmail.com>
2022-11-18 19:29:36 +0100

    switch to 32bit

--- src/boot.asm
+++ src/boot.asm
@@ -1,11 +1,32 @@
 [org 0x7c00]
 
-mov dx, 0xbeef
-call print_hex_16bit
+	mov bp, 0x9000
+	mov sp, bp
 
-jmp $
+	mov bx, MSG_REAL_MODE
+	call print_string
 
-%include "print.asm"
+	call switch_to_pm
+
+	jmp $
+
+%include "src/print/print_16bit.asm"
+%include "src/print/print_32bit.asm"
+%include "src/protected_mode/gdt.asm"
+%include "src/protected_mode/switch_to_32bit.asm"
+
+[bits 32]
+
+BEGIN_PM:
+	mov ebx, MSG_PROT_MODE
+	call print_string_pm
+
+	jmp $
+
+MSG_REAL_MODE db "Started in 16-bit Real mode", 0
+MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode", 0
+
 
 times 510-($-$$) db 0
 dw 0xaa55
+
